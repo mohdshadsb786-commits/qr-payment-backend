a041cf5005798c139e9000a589a98ce4a4af2c7f
mohdshadsb786-commits <mohdshadsb786@gmail.com>
2025-12-09 23:31:34 +0530

    Refactor server.js to remove JSON middleware

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("QR Payment Backend Running Successfully!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
