5a31c97c52b2357ba0844d288a955a50878eea80
mohdshadsb786-commits <mohdshadsb786@gmail.com>
2025-12-09 23:38:04 +0530

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("QR Payment Backend Running Successfully!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
